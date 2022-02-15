c83842d4c048a3670c247f8e5c2743959c3448f2
Cihan Deniz <cih.deniz@gmail.com>
2022-02-15 12:10:13 +0300

    poc for google drive

---
+++ .scripts/HOWTO.md
@@ -0,0 +1,19 @@
+# How to Connect to Google Drive
+
+Follow the steps in these documents;
+
+- https://developers.google.com/drive/api/v3/quickstart/nodejs
+- https://developers.google.com/workspace/guides/create-credentials#service-account
+
+Use this link to select OAuth scope;
+
+- https://developers.google.com/identity/protocols/oauth2/scopes#drive
+
+Select these scopes;
+
+- https://www.googleapis.com/auth/drive.file
+- https://www.googleapis.com/auth/drive.metadata.readonly
+
+Enable drive api;
+
+- https://console.developers.google.com/apis/api/drive.googleapis.com
--- .scripts/index.js
+++ .scripts/index.js
@@ -1,1 +1,49 @@
-console.log('hello world');
+const fs = require('fs');
+const {google} = require('googleapis');
+const {GoogleAuth} = require('google-auth-library');
+const drive = google.drive("v3");
+
+google.options({auth: new GoogleAuth({
+  keyFile: './token.json',
+  scopes: ['https://www.googleapis.com/auth/drive'],
+})});
+
+async function upload() {
+  const fileName = '../assets/logo/logo-circle.png';
+
+  const res = await drive.files.create( {
+      requestBody: {
+        // a requestBody element is required if you want to use multipart
+        name: 'logo-circle.png'
+      },
+      media: {
+        body: fs.createReadStream(fileName),
+      },
+    },{
+      // Use the `onUploadProgress` event from Axios to track the
+      // number of bytes uploaded to this point.
+      onUploadProgress: _ => {
+          console.log('uploading...');
+      },
+    }
+  );
+
+  console.log(res.data);
+}
+
+async function deleteExisting() {
+  const {data} = await drive.files.list({ pageSize: 10 });
+
+    for(const file of data.files) {
+        console.log(`deleting file: ${file.name}`);
+
+        await drive.files.delete({fileId:file.id});
+    }
+}
+
+async function main() {
+    await deleteExisting();
+    await upload();
+}
+
+main();
